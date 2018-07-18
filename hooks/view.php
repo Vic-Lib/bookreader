6774bcda9fccfa2f5806d644bcc0e44696912bb5
leslie-lau <lau@vm-drupal-dev1.vulcan.lan>
2018-07-18 15:41:07 -0400

    Add code to allow users to view books uploaded on resourcespace in the plugin.

--- hooks/view.php
+++ hooks/view.php
@@ -4,20 +4,23 @@ include dirname(__FILE__) . "/../include/utility.php";
 
 function HookBookreaderViewRenderbeforerecorddownload(){
   global $baseurl;
+  //echo buildOptions();
     ?>
       <script>
       var overlay_exists = document.getElementById("br-overlay");
       if (!overlay_exists){
         jQuery("body").append('<div id="br-overlay" style="background-color:rgb(0,0,0); opacity:0.9; width:100%; height:100%; position:fixed; display:none; "></div>');
-        jQuery("body").append('<div id="br-content" style="width:100%; height:100%; position:fixed; display:none;"><iframe id="bookreader-wrapper" src="../plugins/bookreader/include/bookreader_init.php" style="width:80%; height:80%; top:50px; position:relative;"></iframe></div>');
+        jQuery("body").append('<div id="br-content" style="display:none;"></div>');
+
+        //jQuery("body").append('<div id="br-content" style="width:100%; height:100%; position:fixed; display:none;"><iframe id="bookreader-wrapper" src="../plugins/bookreader/include/bookreader_init.php" style="width:80%; height:80%; top:50px; position:relative;"></iframe></div>');
       }
+      
       </script>
     <?php
   }
 
 function HookBookreaderViewReplacepreviewlink() {
   global $baseurl, $resource;
-
 	?>
 
   <div id="previewWrapper">
@@ -25,12 +28,18 @@ function HookBookreaderViewReplacepreviewlink() {
       <script>
         function displayBR(){
           ModalClose();
+          jQuery("#br-content").append('<div id="BookReader" style="width:80%; margin:auto;">Internet Archive BookReader</div>');
           jQuery("#br-overlay").css("display", "block");
-          jQuery("#br-content").css("display", "block");
-          jQuery("#br-content").attr("onclick", "closeBox()"); 
+          jQuery("#br-content").css("display", "block"); 
+          jQuery("#br-overlay").attr("onclick", "closeBox()");
+          
+          var options = <?php echo buildOptions();?>;
+          var br = new BookReader(options);
+          br.init();
         }
 
         function closeBox(){
+            jQuery("#BookReader").remove();
             jQuery("#br-overlay").css("display", "none");
             jQuery("#br-content").css("display", "none");
         }
