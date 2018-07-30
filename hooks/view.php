7320bec097688b544d036615095ec7c91c710fde
leslie-lau <lau@vm-drupal-dev1.vulcan.lan>
2018-07-30 17:10:01 -0400

    Add search capability to a local file.

--- hooks/view.php
+++ hooks/view.php
@@ -4,13 +4,13 @@ include dirname(__FILE__) . "/../include/utility.php";
 
 function HookBookreaderViewRenderbeforerecorddownload(){
   global $baseurl;
-  //echo buildOptions();
     ?>
       <script>
       var overlay_exists = document.getElementById("br-overlay");
       if (!overlay_exists){
         jQuery("body").append('<div id="br-overlay" style="background-color:rgb(0,0,0); opacity:0.9; width:100%; height:100%; position:fixed; display:none; "></div>');
-        jQuery("body").append('<div id="br-content" style="display:none;"></div>');
+
+        jQuery("body").append('<iframe id="br-content" style="width:80%; height:80%; top:50px; margin:auto; position:relative; display:none;" src="../plugins/bookreader/BookReader-source/BookReaderDemo/demo-search.php"></iframe>');
 
         //jQuery("body").append('<div id="br-content" style="width:100%; height:100%; position:fixed; display:none;"><iframe id="bookreader-wrapper" src="../plugins/bookreader/include/bookreader_init.php" style="width:80%; height:80%; top:50px; position:relative;"></iframe></div>');
       }
@@ -20,7 +20,7 @@ function HookBookreaderViewRenderbeforerecorddownload(){
   }
 
 function HookBookreaderViewReplacepreviewlink() {
-  global $baseurl, $resource;
+  global $baseurl, $resource, $ref;
 	?>
 
   <div id="previewWrapper">
@@ -28,14 +28,9 @@ function HookBookreaderViewReplacepreviewlink() {
       <script>
         function displayBR(){
           ModalClose();
-          jQuery("#br-content").append('<div id="BookReader" style="width:80%; margin:auto;">Internet Archive BookReader</div>');
           jQuery("#br-overlay").css("display", "block");
           jQuery("#br-content").css("display", "block"); 
           jQuery("#br-overlay").attr("onclick", "closeBox()");
-          
-          var options = <?php echo buildOptions();?>;
-          var br = new BookReader(options);
-          br.init();
         }
 
         function closeBox(){
@@ -45,6 +40,7 @@ function HookBookreaderViewReplacepreviewlink() {
         }
 
       </script>
+
   </div>
   
 	<?php
