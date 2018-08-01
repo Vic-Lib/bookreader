a6d11c3fb192610dafd2ba31d9ab570fade57e3b
leslie-lau <lau@vm-drupal-dev1.vulcan.lan>
2018-08-01 17:38:11 -0400

    Create test button to initiate BookReader

--- hooks/view.php
+++ hooks/view.php
@@ -3,26 +3,28 @@
 include dirname(__FILE__) . "/../include/utility.php";
 
 function HookBookreaderViewRenderbeforerecorddownload(){
-  global $baseurl;
+  global $baseurl, $ref;
     ?>
+      <form action="../plugins/bookreader/include/bookreader_init.php" method="post">
+        <input type="text" name="rid" value="<?php echo $ref;?>">
+        <input type="submit" value="submit">
+      </form>
+
       <script>
       var overlay_exists = document.getElementById("br-overlay");
       if (!overlay_exists){
         jQuery("body").append('<div id="br-overlay" style="background-color:rgb(0,0,0); opacity:0.9; width:100%; height:100%; position:fixed; display:none; "></div>');
 
-        jQuery("body").append('<iframe id="br-content" style="width:80%; height:80%; top:50px; margin:auto; position:relative; display:none;" src="../plugins/bookreader/BookReader-source/BookReaderDemo/demo-search.php"></iframe>');
+        //jQuery("body").append('<iframe id="br-content" style="width:80%; height:80%; top:50px; margin:auto; position:relative; display:none;" src="../plugins/bookreader/BookReader-source/BookReaderDemo/demo-search.php"></iframe>');
 
         //jQuery("body").append('<div id="br-content" style="width:100%; height:100%; position:fixed; display:none;"><iframe id="bookreader-wrapper" src="../plugins/bookreader/include/bookreader_init.php" style="width:80%; height:80%; top:50px; position:relative;"></iframe></div>');
       }
-      
       </script>
     <?php
   }
 
 function HookBookreaderViewReplacepreviewlink() {
-  global $baseurl, $resource, $ref;
 	?>
-
   <div id="previewWrapper">
     <a id="previewLink" onclick="displayBR()">
       <script>
@@ -40,7 +42,6 @@ function HookBookreaderViewReplacepreviewlink() {
         }
 
       </script>
-
   </div>
   
 	<?php
