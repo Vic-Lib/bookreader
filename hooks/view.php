6684f3de7fae258e9f7ea627cdae6cb419e95fd2
leslie-lau <lau@vm-drupal-dev1.vulcan.lan>
2018-08-07 15:33:54 -0400

    Move the test button's work into the pdf image.

--- hooks/view.php
+++ hooks/view.php
@@ -1,25 +1,25 @@
 <?php
 
-include dirname(__FILE__) . "/../include/utility.php";
-
 function HookBookreaderViewRenderbeforerecorddownload(){
   global $baseurl, $ref;
     ?>
-      <form action="../plugins/bookreader/include/bookreader_init.php" method="post">
-        <input type="text" name="rid" value="<?php echo $ref;?>">
-        <input type="submit" value="submit">
-      </form>
+    <form id="resourceId" action="../plugins/bookreader/include/bookreader_init.php" target="br-content" method="post">
+      <input type="hidden" name="rid" value="<?php echo $ref;?>">
+    </form>
+    
+    <script type="text/javascript">
+      jQuery(document).ready(function(){
+        jQuery("#resourceId").submit();
+      });
 
-      <script>
       var overlay_exists = document.getElementById("br-overlay");
       if (!overlay_exists){
-        jQuery("body").append('<div id="br-overlay" style="background-color:rgb(0,0,0); opacity:0.9; width:100%; height:100%; position:fixed; display:none; "></div>');
+        jQuery("body").append('<div id="br-overlay" onclick="closeBR()" style="background-color:rgb(0,0,0); opacity:0.9; width:100%; height:100%; position:fixed; display:none;"></div>');
 
-        //jQuery("body").append('<iframe id="br-content" style="width:80%; height:80%; top:50px; margin:auto; position:relative; display:none;" src="../plugins/bookreader/BookReader-source/BookReaderDemo/demo-search.php"></iframe>');
-
-        //jQuery("body").append('<div id="br-content" style="width:100%; height:100%; position:fixed; display:none;"><iframe id="bookreader-wrapper" src="../plugins/bookreader/include/bookreader_init.php" style="width:80%; height:80%; top:50px; position:relative;"></iframe></div>');
+        jQuery("body").append('<iframe id="br-content" name="br-content" style="width:80%; height:80%; top:50px; margin:auto; position:relative; display:none;" src="../plugins/bookreader/include/bookreader_init.php"></iframe>');
       }
-      </script>
+    </script>
+
     <?php
   }
 
@@ -27,26 +27,23 @@ function HookBookreaderViewReplacepreviewlink() {
 	?>
   <div id="previewWrapper">
     <a id="previewLink" onclick="displayBR()">
-      <script>
-        function displayBR(){
-          ModalClose();
-          jQuery("#br-overlay").css("display", "block");
-          jQuery("#br-content").css("display", "block"); 
-          jQuery("#br-overlay").attr("onclick", "closeBox()");
-        }
+      
+  <script>
+    function displayBR(){
+      ModalClose();
+      jQuery("#br-overlay").css("display", "block");
+      jQuery("#br-content").css("display", "block");
+    }
 
-        function closeBox(){
-            jQuery("#BookReader").remove();
-            jQuery("#br-overlay").css("display", "none");
-            jQuery("#br-content").css("display", "none");
-        }
+    function closeBR(){
+      jQuery("#BookReader").remove();
+      jQuery("#br-overlay").css("display", "none");
+      jQuery("#br-content").css("display", "none");
+    }
+  </script>
 
-      </script>
-  </div>
-  
 	<?php
 	return true;
 }
-
 
 ?>
