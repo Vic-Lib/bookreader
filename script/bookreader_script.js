7186abba3b3365e98f290acdc5229337c1f36ba6
leslie-lau <leslie.kc.lau@gmail.com>
2018-08-27 15:14:08 -0400

    Include more gradual zoom in/out options, update banner img

--- script/bookreader_script.js
+++ script/bookreader_script.js
@@ -1,5 +1,5 @@
 //
-// This file shows the minimum you need to provide to BookReader to display a book
+// This file provides the required info to BookReader to display a book
 //
 // Copyright(c)2008-2009 Internet Archive. Software license AGPL version 3.
 
@@ -55,10 +55,26 @@ var options = {
         return index+1;
     },
 
+    // embed, full (responsive)
+    ui: 'full',
+
+    showLogo: false,
+
     // Override the path used to find UI images
     imagesBaseURL: '../BookReader-source/BookReader/images/',
-    // Note previously the UI param was used for mobile, but it's going to be responsive
-    // embed === iframe
+
+    // Adjust zoom Levels, (reduce < 1 = zoom in)
+    reductionFactors: [
+        {reduce: 0.5, autofit: null},
+        {reduce: 0.8, autofit: null},
+        {reduce: 1,   autofit: null},
+        {reduce: 1.3, autofit: null},
+        {reduce: 2, autofit: null},
+        {reduce: 4, autofit: null},
+        {reduce: 6, autofit: null}
+    ],
+
+    // Values for the "About this book" popup
     metadata: [
         {label: 'Resource ID', value: rid},
         {label: 'Title', value: title},
@@ -67,13 +83,11 @@ var options = {
     ],
     thumbnail: url_list[0],
 
-    showLogo: false,
-    
+
+    /* BookReader's search plugin options */
+    server: rs_dir,
     bookId: rid,
     bookPath: path_to_pdf,
-    ui: 'full', // embed, full (responsive)
-
-    server: rs_dir,
     searchInsideUrl: "/plugins/bookreader/search_inside.php",
 
 };
